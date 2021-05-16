d322e44417810aac78d1f629a7f3bcdc6c15f330
fauzanakmalh1 <fauzanakmalhariz1@gmail.com>
2021-05-17 00:33:48 +0700

    update pages & data

---
+++ src/Pages/home.js
@@ -0,0 +1,457 @@
+import React, { useState } from "react";
+import axios from "axios";
+import qs from "qs";
+
+export default function Home() {
+  
+  const [value, setValue] = useState({
+    codes: [],
+    title: '',
+    description: '',
+    author: '',
+    url: '',
+    language: '',
+    framework: '',
+    year: ''
+  });
+
+  const getData = async () => {
+    const BASE_URL = "http://localhost:3030/codes/query";
+    // API server : https://lib-it.herokuapp.com/codes/query;
+    // API local : http://localhost:3030/codes/query;
+
+    const headers = {
+      'Accept': 'application/sparql-results+json,*/*;q=0.9',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    };
+
+    const queryData = {
+      query: `PREFIX data: <https://github.com/repo#>
+      PREFIX id: <https://github.com/akun#>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      SELECT ?title ?description ?author ?url ?language ?framework ?year
+      WHERE
+      {
+        ?id data:title ?title ;
+            data:description ?description ;
+            data:author ?author ;
+            data:url ?url ;
+            data:hasLanguage ?nameLanguage ;
+            data:hasFramework ?nameFramework ;
+            data:hasYear ?nameYear .
+            
+            ?nameLanguage data:language ?language .
+            ?nameFramework data:framework ?framework .
+            ?nameYear data:year ?year .
+            
+            FILTER contains(lcase(str(?title)), lcase(str("${value.title ? value.title : ''}")))
+            FILTER contains(lcase(str(?description)), lcase(str("${value.description ? value.description : ''}")))
+            FILTER contains(lcase(str(?author)), lcase(str("${value.author ? value.author : ''}")))
+            FILTER contains(lcase(str(?url)), lcase(str("${value.url ? value.url : ''}")))
+            FILTER contains(lcase(str(?language)), lcase(str("${value.language ? value.language : ''}")))
+            FILTER contains(lcase(str(?framework)), lcase(str("${value.framework ? value.framework : ''}")))
+            FILTER contains(lcase(str(?year)), lcase(str("${value.year ? value.year : ''}")))
+      }`,
+    };
+
+    try {
+      const { data } = await axios(BASE_URL, {
+        method: "POST",
+        headers,
+        data: qs.stringify(queryData),
+      });
+      console.log(data);
+
+      // Convert Data
+      const formatted_data = data.results.bindings.map((code, index) =>
+        formatter(code, index)
+      );
+      console.log(formatted_data);
+
+      setValue({
+        ...value,
+        codes: formatted_data,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const formatter = (codes, index) => {
+    return {
+      "id": index,
+      "title": codes.title.value,
+      "description": codes.description.value,
+      "author": codes.author.value,
+      "url": codes.url.value,
+      "language": codes.language.value,
+      "framework": codes.framework.value,
+      "year": codes.year.value
+    }
+  }
+
+  const handleChangeTitle = event => {
+    setValue({
+      ...value,
+      'title': event.target.value,
+    });
+  }
+
+    const handleChangeAuthor = event => {
+    setValue({
+      ...value,
+      'author': event.target.value,
+    });
+  }
+
+  const handleChangeLanguage = event => {
+    setValue({
+      ...value,
+      'language': event.target.value,
+    });
+  }
+
+  const handleChangeFramework = event => {
+    setValue({
+      ...value,
+      'framework': event.target.value,
+    });
+  }
+
+  const handleChangeYear = event => {
+    setValue({
+      ...value,
+      'year': event.target.value,
+    });
+  }
+
+  const content = value.codes.map((code) =>
+  <div className="codes_list_container text-justify">
+    <ul>
+      <li key={code.id}>
+        <div className="d-flex flex-row align-items-start justify-content-start"></div>
+        <div className="code_info"></div>
+        <div className="code_title">
+          {code.title}
+          <br />
+        </div>
+        <div className="code_language">
+          Language : {code.language}
+          <br />
+        </div>
+        Framework : {code.framework}
+        <br />
+        Year : {code.year}
+        <br />
+        Description : {code.description}
+        <br />
+        Author : {code.author}
+        <br />
+        URL :{" "}
+        <a href={code.url} target="_blank" rel="noreferrer">
+          {code.url}
+        </a>
+        <br />
+        <hr className="line-style"></hr>
+        <div className="margin-style"></div>
+      </li>
+    </ul>
+    <ul className="codes_list"></ul>
+  </div>
+  )
+
+  return (
+    <div className="super_container">
+      {/* Header */}
+      <header className="header">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="header_content d-flex flex-row align-items-center justify-content-center">
+                {/* Logo */}
+                <div className="logo mr-auto">
+                  <div className="d-flex flex-row align-items-end justify-content-start">
+                    <span className="logo_text">
+                      <a href="/">LibIT!</a>
+                    </span>
+                  </div>
+                </div>
+                <div className="ml-auto">
+                  <div className="d-flex flex-row align-items-end justify-content-end">
+                    <span className="nav_about_text">
+                      <a href="#about">About</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Home */}
+      <div className="home" id="home">
+        {/* Home Slider */}
+        <div className="home_slider_container">
+          <div className="owl-carousel owl-theme home_slider">
+            {/* Slide */}
+            <div className="slide">
+              <div
+                className="background_image"
+                style={{ backgroundImage: "url(images/img-home.jpg)" }}
+              />
+              <div className="home_container">
+                <div className="container">
+                  <div className="row">
+                    <div className="col">
+                      <div className="home_content">
+                        <div className="home_title_container text-center">
+                          <div className="home_title islive text-center">
+                            <h1>
+                              Lib <span>IT!</span>
+                            </h1>
+                          </div>
+                        </div>
+                        <div className="code_form_container">
+                          <form
+                            className="code_form"
+                            onSubmit={(e) => e.preventDefault()}
+                          >
+                            <div className="align-items-center justify-content-center">
+                              <div className="row">
+                                <div className="col-md-6">
+                                  <div className="code_form_inputs align-items-center justify-content-center">
+                                    <input
+                                      type="text"
+                                      className="code_form_input"
+                                      placeholder="Title"
+                                      setvalue={value.title}
+                                      onChange={handleChangeTitle}
+                                    />
+                                  </div>
+                                </div>
+                                <div className="col-md-6">
+                                  <div className="code_form_inputs align-items-center justify-content-center">
+                                    <input
+                                      type="text"
+                                      className="code_form_input"
+                                      placeholder="Author"
+                                      setvalue={value.author}
+                                      onChange={handleChangeAuthor}
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="row">
+                                <div className="col-md-6">
+                                  <div className="code_form_inputs align-items-center justify-content-center">
+                                    <input
+                                      type="text"
+                                      className="code_form_input"
+                                      placeholder="Language"
+                                      setvalue={value.language}
+                                      onChange={handleChangeLanguage}
+                                    />
+                                  </div>
+                                </div>
+                                <div className="col-md-6">
+                                  <div className="code_form_inputs align-items-center justify-content-center">
+                                    <input
+                                      type="text"
+                                      className="code_form_input"
+                                      placeholder="Framework"
+                                      setvalue={value.framework}
+                                      onChange={handleChangeFramework}
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="row">
+                                <div className="col-md-12">
+                                  <div className="code_form_inputs align-items-center justify-content-center">
+                                    <select setvalue={value.year} className="dropdown code_form_input" onChange={handleChangeYear}>
+                                      <option value="">Year</option>
+                                      <option value="2013">2013</option>
+                                      <option value="2015">2015</option>
+                                      <option value="2016">2016</option>
+                                      <option value="2017">2017</option>
+                                      <option value="2018">2018</option>
+                                      <option value="2019">2019</option>
+                                      <option value="2020">2020</option>
+                                      <option value="2021">2021</option>
+                                    </select>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="row">
+                                <div className="col-md-12">
+                                  <button
+                                    type="button"
+                                    className="code_form_button button"
+                                    value="Search"
+                                    onClick={getData}
+                                  >
+                                    <span>Search</span>
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Codes */}
+      <div className="codes">
+        <div className="container">
+          <div className="row row-lg-eq-height">
+            {/* Add */}
+            <div className="col-lg-4 order-lg-1 order-2 codes_col">
+              <div className="extra d-flex flex-column align-items-center justify-content-between">
+                <div
+                  className="background_image"
+                  style={{ backgroundImage: "url(images/img-result.jpg)" }}
+                />
+                <div className="extra_frame" />
+                <div className="extra_text">Source Code</div>
+                <div className="extra_title_container">
+                  <div className="extra_year">2021</div>
+                  <div className="extra_title">Result</div>
+                  <div className="extra_subtitle">Repositories</div>
+                </div>
+              </div>
+            </div>
+            {/* Codes Content */}
+            <div className="col-lg-8 order-lg-2 order-1">
+              <div className="codes_content">
+                <div className="section_title">
+                  <h1 className="result-text">Result</h1>
+                  <div className="margin-style"></div>
+                </div>
+                {content}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* About */}
+      <div className="about" id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="section_title">
+                <h1>About</h1>
+              </div>
+            </div>
+          </div>
+          <div className="row about_row">
+            {/* About Post */}
+            <div className="col-xl-4 col-md-6">
+              <div className="about_post image_top">
+                <div className="about_post_image">
+                  <img src="images/img-semantic-web.png" alt="" />
+                </div>
+                <div className="about_post_content">
+                  <div className="about_post_title">
+                    <span>Semantic Web?</span>
+                  </div>
+                  <div className="about_post_text">
+                    <p>
+                      The Semantic Web is an extension of the World Wide Web
+                      through standards set by the World Wide Web Consortium
+                      (W3C). The goal of the Semantic Web is to make Internet
+                      data machine-readable. To enable the encoding of semantics
+                      with the data, technologies such as Resource Description
+                      Framework (RDF) and Web Ontology Language (OWL) are used.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* About Post */}
+            <div className="col-xl-4 col-md-6">
+              <div className="about_post image_bottom d-flex flex-column align-items-start justify-content-start">
+                <div className="about_post_content order-md-1 order-2">
+                  <div className="about_post_title">
+                    <span>What is LibIT?</span>
+                  </div>
+                  <div className="about_post_text">
+                    <p>
+                      LibIT! is a website that you can help you to find project
+                      references with many programming language and framework
+                      development, we had choosen the best repositories from
+                      github and other sources only for you. This website build
+                      with ExpressJS, ReactJS and SPARQL.
+                    </p>
+                  </div>
+                </div>
+                <div className="about_post_image order-md-2 order-1">
+                  <img src="images/img-about-lib-it.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+            {/* about Post */}
+            <div className="col-xl-4 col-md-6 margin-top-style">
+              <div className="about_post image_top">
+                <div className="about_post_image">
+                  <img src="images/img-about-developer.png" alt="" />
+                </div>
+                <div className="about_post_content">
+                  <div className="about_post_title">
+                    <span>Developer?</span>
+                  </div>
+                  <div className="about_post_text">
+                    <p>
+                      140810180005 - Fauzan Akmal Hariz <br />
+                      140810180013 - Alvin
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Footer */}
+      <footer className="footer">
+        <div
+          className="background_image"
+          style={{ backgroundImage: "url(images/img-footer.png)" }}
+        />
+        <div className="footer_content">
+          <div className="container">
+            <div className="row">
+              <div className="col text-center">
+                {/* Logo */}
+                <div className="logo">
+                  <div className="d-flex flex-row align-items-end justify-content-start">
+                    <p className="logo_text">LibIT!</p>
+                  </div>
+                </div>
+                {/* Footer Social */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer_bar">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div className="copyright text-center">
+                  Copyright &copy; All rights reserved - LibIT!
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
